d8c67d1a605c66e03934291bcf746837ac7a061d
ojhankit <ojha27ankit@gmail.com>
2025-10-06 11:50:57 +0000

    task serializer

---
+++ apps/tasks/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Task
+from django.utils import timezone
+
+class TaskSerializer(serializers.ModelSerializer):
+    assigned_by = serializers.StringRelatedField(read_only=True)  # show username/email
+    assigned_to = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=None  # will set dynamically in view
+    )
+    
+    class Meta:
+        model = Task
+        fields = [
+            'id',
+            'title',
+            'description',
+            'docs',
+            'assigned_by',
+            'assigned_to',
+            'deadline',
+            'assigned_date',
+            'labels'
+        ]
+        read_only_fields = ['assigned_by', 'assigned_date']
+
+    def create(self, validated_data):
+        assigned_to_data = validated_data.pop('assigned_to')
+        task = Task.objects.create(**validated_data, assigned_by=self.context['request'].user)
+        task.assigned_to.set(assigned_to_data)
+        return task
